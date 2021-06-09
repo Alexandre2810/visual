5021833181883e540dcf87dd665189ca41b8f2cf
lorde78 <62546853+lorde78@users.noreply.github.com>
2021-06-09 17:48:36 +0200

    ok

--- data.php
+++ data.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json');
 
 $conn = mysqli_connect("127.0.0.1", "root", "", "projetfinal");
 
-$sqlQuery = "SELECT joueurs,score FROM data___feuille_3 ORDER BY joueurs";
+$sqlQuery = "SELECT joueurs,score FROM data___feuille_3";
 
 $result = mysqli_query($conn, $sqlQuery);
 
