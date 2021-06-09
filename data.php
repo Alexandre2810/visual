a395c01c85cbd6592f5c63d13b9b95de66669bda
lorde78 <62546853+lorde78@users.noreply.github.com>
2021-06-09 15:11:29 +0200

    push

--- data.php
+++ data.php
@@ -1,13 +1,9 @@
 <?php
 header('Content-Type: application/json');
 
-#conn
-
 $conn = mysqli_connect("localhost","root","","projetfinal");
 
-#sqlQuery
-
-$sqlQuery = "SELECT joueurs,score,exp1,exp2,exp3,exp4 FROM projetfinal.data___feuille_3; ORDER BY joueurs";
+$sqlQuery = "SELECT joueurs,score FROM data__feuille_3";
 
 $result = mysqli_query($conn,$sqlQuery);
 
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Creating Dynamic Data Graph using PHP and Chart.js</title>
+<style type="text/css">
+BODY {
+    width: 550PX;
+}
+
+#chart-container {
+    width: 100%;
+    height: auto;
+}
+</style>
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/Chart.min.js"></script>
+
+
+</head>
+<body>
+    <div id="chart-container">
+        <canvas id="graphCanvas"></canvas>
+    </div>
+
+    <script>
+        $(document).ready(function () {
+            showGraph();
+        });
+
+        function showGraph()
+        {
+            {
+                $.post("data.php",
+                function (data)
+                {
+                    console.log(data);
+                     var joueurs = [];
+                    var score = [];
+
+                    for (var i in data) {
+                        joueurs.push(data[i].joueurs);
+                        score.push(data[i].score);
+                    }
+
+                    var chartdata = {
+                        labels: joueurs,
+                        datasets: [
+                            {
+                                label: 'Student Marks',
+                                backgroundColor: '#49e2ff',
+                                borderColor: '#46d5f1',
+                                hoverBackgroundColor: '#CCCCCC',
+                                hoverBorderColor: '#666666',
+                                data: score
+                            }
+                        ]
+                    };
+
+                    var graphTarget = $("#graphCanvas");
+
+                    var barGraph = new Chart(graphTarget, {
+                        type: 'bar',
+                        data: chartdata
+                    });
+                });
+            }
+        }
+        </script>
+
+</body>
+</html>
