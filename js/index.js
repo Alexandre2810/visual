bb96ba573c0466aba9a06f867c1323d9acb4ef89
Wylohn <wylohn.tribout@gmail.com>
2021-06-14 23:06:16 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,11 +2,6 @@ const text1 = document.querySelector('.acuity_text')
 const text2 = document.querySelector('.test2_text')
 const text3 = document.querySelector('.test3_text')
 
-var button
-var m
-var mo
-var mon
-
 // const button0 = document.querySelector('#part0')
 const button1 = document.querySelector('#part1')
 const button2 = document.querySelector('#part2')
@@ -14,31 +9,28 @@ const button3 = document.querySelector('#part3')
 
 
 // button0.addEventListener('click', nbPart(button0))
-button1.addEventListener('click', nbPart(m))
-button2.addEventListener('click', nbPart(mo))
-button3.addEventListener('click', nbPart(mon))
+button1.addEventListener('click', Part1)
+button2.addEventListener('click', Part2)
+button3.addEventListener('click', Part3)
 
 
 
 
-function nbPart (button) {
-    if (button == m) {
-        text1.classList.add('hide_acuity_text')
-        text2.classList.remove('hide_test2_text')
-        text3.classList.remove('hide_test3_text')
-    };
-
-    if (button == mo) {
-        text1.classList.remove('hide_acuity_text')
-        text2.classList.add('hide_test2_text')
-        text3.classList.remove('hide_test3_text')
-    };
-
-    if (button == mon) {
-        text1.classList.remove('hide_acuity_text')
-        text2.classList.remove('hide_test2_text')
-        text3.classList.add('hide_test3_text')
-    }
+function Part1 () {
+    text1.classList.add('hide_acuity_text')
+    text2.classList.remove('hide_test2_text')
+    text3.classList.remove('hide_test3_text')        
 }
 
 
+function Part2 () {    
+    text1.classList.remove('hide_acuity_text')
+    text2.classList.add('hide_test2_text')
+    text3.classList.remove('hide_test3_text')
+}
+
+function Part3 () {
+    text1.classList.remove('hide_acuity_text')
+    text2.classList.remove('hide_test2_text')
+    text3.classList.add('hide_test3_text')
+}
