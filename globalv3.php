80d20886f11f5ab2f7bf1440e26a043653bee00d
Wylohn <wylohn.tribout@gmail.com>
2021-06-16 17:58:23 +0200

    Update globalv3.php

--- globalv3.php
+++ globalv3.php
@@ -55,94 +55,7 @@
         </picture>
 
     </div>
-    <script>
-        $(document).ready(function() {
-            showGraph();
-        });
-
-
-
-        function showGraph() {
-            {
-                $.get("data.php",
-                    function(data) {
-                        console.log(data);
-                        var joueurs = [];
-                        var score = [];
-
-                        for (var i in data) {
-                            joueurs.push(data[i].joueurs);
-                            score.push(parseFloat(data[i].score.replace(',', '.')));
-                        }
-                        console.log(score)
-                        var chartdata = {
-                            labels: ['Gamers', 'Non-Gamers', 'Exp1', 'Exp2', 'Exp3', 'Exp4'],
-                            datasets: [{
-                                label: 'Visual Acuity Score',
-                                data: score,
-                                backgroundColor: [
-                                    'rgba(255, 99, 132, 0.2)',
-                                    'rgba(54, 162, 235, 0.2)',
-                                    'rgba(255, 206, 86, 0.2)',
-                                    'rgba(75, 192, 192, 0.2)',
-                                    'rgba(153, 102, 255, 0.2)',
-                                    'rgba(255, 159, 64, 0.2)'
-                                ],
-                                borderColor: [
-                                    'rgba(255, 99, 132, 1)',
-                                    'rgba(54, 162, 235, 1)',
-                                    'rgba(255, 206, 86, 1)',
-                                    'rgba(75, 192, 192, 1)',
-                                    'rgba(153, 102, 255, 1)',
-                                    'rgba(255, 159, 64, 1)'
-                                ],
-                                hoverBackgroundColor: '#CCCCCC',
-                                hoverBorderColor: '#666666',
-                                borderWidth: 1
-                            }]
-                        };
-
-                        var graphTarget = $("#acuity");
-
-                        var barGraph = new Chart(graphTarget, {
-                            type: 'bar',
-                            data: chartdata,
-                            options: {
-                                animation: {
-                                    duration: 4000,
-                                    easing: 'easeInBounce',
-                                },
-                                legend: {
-                                    labels: {
-                                        fontColor: 'rgb(255, 255, 255)',
-                                        fontSize: 30,
-                                    }
-                                },
-                                scales: {
-                                    yAxes: [{
-                                        ticks: {
-                                            fontSize: 14,
-                                            fontColor: "white",
-                                            beginAtZero: true
-                                        }
-                                    }],
-                                    xAxes: [{
-                                        ticks: {
-                                            fontColor: "white",
-                                            fontSize: 14,
-                                            stepSize: 1,
-                                            beginAtZero: true
-                                        }
-                                    }]
-                                }
-                            }
-
-                        });
-                    });
-            }
-
-        }
-    </script>
+    
 
 </body>
 <footer>
@@ -155,6 +68,6 @@
     </nav>
 </footer>
 
-<script src="js/index.js"></script>
+<script src="js/test.js"></script>
 
 </html>
